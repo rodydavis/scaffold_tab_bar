1b20c9266a85b06ee36a780fd6598bc7352d04e1
Rody Davis <rody.davis.jr@gmail.com>
2019-08-23 10:58:50 -0400

    adding web

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:flutter_web/material.dart';
 
 import 'package:scaffold_tab_bar/scaffold_tab_bar.dart';
 import 'package:scaffold_tab_bar_example/screens/index.dart';
